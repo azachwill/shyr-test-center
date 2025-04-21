2edd027dabff0489121dd9c722511f833ae5f35e
awilliams1275 <andrew.williams@rstudio.com>
2025-04-21 16:42:31 -0400

    devcommit04218

--- netlify/edge-functions/redirect_article.js
+++ netlify/edge-functions/redirect_article.js
@@ -14,38 +14,39 @@ export default async (request, context) => {
     "/py/api/core",
     "/py/api/express",
     "/py/api/testing"
-    ];
-    const pattern = new RegExp(patternArray.join("|"));
-    const result = pattern.test(url);
+  ];
+  const pattern = new RegExp(patternArray.join("|"));
+  const result = pattern.test(url);
 
-    //exclude the following from redirect_article processing:
-    const patternArrayNot = [
-      "/r/reference/shiny/(\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?|latest)(\\/?)$",
-      //shiny.posit.co/py/ directories:
-      "/py/api/core",
-      "/py/api/express",
-      "/py/api/testing"
-      ];
-      const patternNot = new RegExp(patternArrayNot.join("|"));
-      const resultNot = patternNot.test(url);
+  //exclude the following from redirect_article processing:
+  const patternArrayNot = [
+    "/r/reference/shiny/(\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?|latest)(\\/?)$",
+    //shiny.posit.co/py/ directories:
+    "/py/api/core(\\/?)$",
+    "/py/api/express(\\/?)$",
+    "/py/api/testing(\\/?)$"
+  ];
+  const patternNot = new RegExp(patternArrayNot.join("|"));
+  const resultNot = patternNot.test(url.pathname);
 
-     // Check if the URL path is one of the index.html pages
-    if ( result && !resultNot &&  !url.includes(".html") === false){
-      // Remove the trailing slash and add .html extension
-      const newPathname = url.pathname.endsWith("/") ? url.pathname.slice(0, -1) + '.html' : url.pathname + '.html';
-      const newUrl = `${url.origin}${newPathname}`;
-
-        // Perform a 301 redirect
-      return new Response(null, {
-        status: 301,
-        headers: {
-          'Location': newUrl,
-        },
-      });
-    }
-     // If the URL does not match the criteria, proceed with the request as usual
+  // If the URL does not match the criteria, proceed with the request as usual
+  if (!result || resultNot || url.pathname.endsWith(".html")) {
     return context.next();
-  };
-   export const config = {
-    path: ['/r/*','/py/*']
-  };
+  }
+
+  // Remove the trailing slash and add .html extension
+  const newPathname = url.pathname.endsWith("/") ? url.pathname.slice(0, -1) + '.html' : url.pathname + '.html';
+  const newUrl = `${url.origin}${newPathname}${url.search}${url.hash}`;
+
+  // Perform a 301 redirect
+  return new Response(null, {
+    status: 301,
+    headers: {
+      'Location': newUrl,
+    },
+  });
+};
+
+export const config = {
+  path: ['/r/*', '/py/*'],
+};
